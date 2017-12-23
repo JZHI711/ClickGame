640e8258384c717b53c1c2b2b9f8de9541690587
4419-2-8\user <s1410422032@gms.nutc.edu.tw>
2017-12-23 10:46:42 +0800

    1.新增EnemyBehavior 2.完成FadeIn/Out

---
+++ Assets/Script/View/Behavior/EnemyBehavoir.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Animator))]//屬性，作描述;要求script要有animator存在才能執行
+
+public class EnemyBehavoir : MonoBehaviour {
+    private Animator animator;
+	// Use this for initialization
+	private void Awake () {
+        animator = GetComponent<Animator>();
+        animator.SetTrigger("die");
+    }
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Script/View/Component/MeshFader.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshFader : MonoBehaviour {
+    public Renderer[] fadeRenderers;
+    public float speed = 1f;
+    [ContextMenu("Test FadeIn")]
+    private void Test()
+    {
+        StopCoroutine(FadeOut());//防止同時執行FadeIn/Out
+        StartCoroutine(FadeIn());
+    }
+    [ContextMenu("Test FadeOut")]
+    private void TestFadeOut()
+    {
+        StopCoroutine(FadeIn());
+        StartCoroutine(FadeOut());
+    }
+    public IEnumerator FadeIn()
+    {
+        float alpha = 0;
+        ChangeAlpha(alpha);
+        while (alpha < 1)
+        {
+            alpha += Time.deltaTime * speed;
+            alpha = Mathf.Min(1,alpha);//取最小值
+            ChangeAlpha(alpha);
+            yield return null;
+        }
+        
+
+    }
+    public IEnumerator FadeOut()
+    {
+        float alpha = 1;
+        ChangeAlpha(alpha);
+        while (alpha >0)
+        {
+            alpha -= Time.deltaTime * speed;
+            alpha = Mathf.Max(0, alpha);
+            ChangeAlpha(alpha);
+            yield return null;
+        }
+    }
+    private void ChangeAlpha(float alpha)
+    {
+        for (int i = 0; i < fadeRenderers.Length; i++)
+        {
+            Color color = fadeRenderers[i].material.color;//color要先指定
+            color.a = alpha;
+            fadeRenderers[i].material.color = color;
+        }
+    }
+}
