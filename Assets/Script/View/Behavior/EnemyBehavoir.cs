721d706194e184586cc0118091511d61a9eceffd
4419-2-8\user <s1410422032@gms.nutc.edu.tw>
2017-12-23 11:01:14 +0800

    1.EnemyHurt

--- Assets/Script/View/Behavior/EnemyBehavoir.cs
+++ Assets/Script/View/Behavior/EnemyBehavoir.cs
@@ -2,20 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 [RequireComponent(typeof(Animator))]//屬性，作描述;要求script要有animator存在才能執行
+[RequireComponent(typeof(MeshFader))]
+[RequireComponent(typeof(AudioSource))]
 
 public class EnemyBehavoir : MonoBehaviour {
     private Animator animator;
+    private MeshFader meshFader;
+    private AudioSource audioSource;
+    public AudioClip hurtClip;
 	// Use this for initialization
 	private void Awake () {
         animator = GetComponent<Animator>();
-        animator.SetTrigger("die");
+        meshFader = GetComponent<MeshFader>();
+        audioSource = GetComponent<AudioSource>();
     }
-    void Start()
+    private void OnEnable()
     {
+        StartCoroutine(meshFader.FadeIn());
+    }
+    public void DoDemage(float attack) {
+        animator.SetTrigger("hurt");
+        audioSource.clip = hurtClip;
+        audioSource.Play();
+    }
+    private void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+
+            DoDemage(10);
+        }
 
     }
-    // Update is called once per frame
-    void Update () {
-		
-	}
+  
 }
